33a78f59476426d38270ad7b15e3d965c907dc1c
Farhad Hossain <farhadalam643.com>
2022-04-23 15:42:01 +0600

    landfolio

--- components/Nav.js
+++ components/Nav.js
@@ -5,8 +5,9 @@ import { navData } from "../data/data";
 const Nav = () => {
   return (
     <div className="fixed flex items-center cssGlassNav px-6 md:px-10 rounded-full py-[.4rem] md:py-[.6rem] bottom-6 md:bottom-12 left-[50%] translate-x-[-50%] space-x-6 z-10">
-      {navData.map((item) => (
+      {navData.map((item, index) => (
         <Link
+          id={index}
           activeClass="navDiv"
           to={item.title}
           spy={true}
